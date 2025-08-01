dd6eec54078a5ac1f14f110a1ca902afd124366b
mk master <79350888+MKmasterg@users.noreply.github.com>
2025-08-01 16:30:14 +0330

    Add create users table at the begining of the server

--- src/server.java
+++ src/server.java
@@ -4,18 +4,18 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
     private static final int PORT = 12345;
-    private static final String DB_URL = "jdbc:sqlite:users.db";
 
     // Map of currently connected clients
     private static ConcurrentHashMap<String, ClientHandler> clients = new ConcurrentHashMap<>();
 
     public static void main(String[] args) throws Exception {
+        db.server.Utils.createUsersTable();
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             System.out.println("Server started on port " + PORT);
 
             while (true) {
                 Socket clientSocket = serverSocket.accept();
-                new Thread(new ClientHandler(clientSocket, DB_URL, clients)).start();
+                new Thread(new ClientHandler(clientSocket, clients)).start();
             }
         }
     }
